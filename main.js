6765f25a5bff66d3553f5822b652487ca7c639f6
markcaf <marcocafassog@gmail.com>
2022-06-30 17:12:29 +0200

    Milestone 1, print feed posts dynamically

--- main.js
+++ main.js
@@ -96,3 +96,42 @@ const posts = [
         "created": "2021-12-11"
     }
 ];
+
+// Recupero il parent in cui verranno inseriti i post
+const postWrapper = document.getElementById('container');
+
+for ( let i = 0; i < posts.length; i++){
+    const currentPost = posts[i];
+
+    postWrapper.innerHTML += ` 
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${currentPost.author.image}" alt="${currentPost.author.name}">
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${currentPost.author.name}</div>
+                    <div class="post-meta__time">${currentPost.created}</div>
+                </div>
+            </div>
+        </div>
+        <div class="post__text">${currentPost.content}</div>
+        <div class="post__image">
+            <img src="${currentPost.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${currentPost.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${currentPost.likes}</b> persone
+                </div>
+            </div>
+        </div>
+    </div> `;
+}
